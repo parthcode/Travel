7d66463d122cf95e64f50ba11d95b1f1e286a36d
parthcode <parthsit@gmail.com>
2015-08-13 15:36:32 +0530

    Create Airport.java

---
+++ Airport.java
@@ -0,0 +1,116 @@
+
+package rome2rio;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Airport {
+
+    private String code;
+    private String name;
+    private String pos;
+    private String countryCode;
+    private String timeZone;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * 
+     * @param code
+     *     The code
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The pos
+     */
+    public String getPos() {
+        return pos;
+    }
+
+    /**
+     * 
+     * @param pos
+     *     The pos
+     */
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+
+    /**
+     * 
+     * @return
+     *     The countryCode
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * 
+     * @param countryCode
+     *     The countryCode
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    /**
+     * 
+     * @return
+     *     The timeZone
+     */
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    /**
+     * 
+     * @param timeZone
+     *     The timeZone
+     */
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
