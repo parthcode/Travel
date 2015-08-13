06648c5a86ab340dc45d84a2e743d76335c23a35
parthcode <parthsit@gmail.com>
2015-08-13 15:44:59 +0530

    Create Place.java

---
+++ Place.java
@@ -0,0 +1,135 @@
+
+package rome2rio;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Place {
+
+    private String kind;
+    private String name;
+    private String longName;
+    private String pos;
+    private String countryCode;
+    private String regionCode;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The kind
+     */
+    public String getKind() {
+        return kind;
+    }
+
+    /**
+     * 
+     * @param kind
+     *     The kind
+     */
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The longName
+     */
+    public String getLongName() {
+        return longName;
+    }
+
+    /**
+     * 
+     * @param longName
+     *     The longName
+     */
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    /**
+     * 
+     * @return
+     *     The pos
+     */
+    public String getPos() {
+        return pos;
+    }
+
+    /**
+     * 
+     * @param pos
+     *     The pos
+     */
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+
+    /**
+     * 
+     * @return
+     *     The countryCode
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * 
+     * @param countryCode
+     *     The countryCode
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    /**
+     * 
+     * @return
+     *     The regionCode
+     */
+    public String getRegionCode() {
+        return regionCode;
+    }
+
+    /**
+     * 
+     * @param regionCode
+     *     The regionCode
+     */
+    public void setRegionCode(String regionCode) {
+        this.regionCode = regionCode;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
