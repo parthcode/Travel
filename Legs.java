f11b419d4f4afe03545c34d8e58f100a0cc05c65
parthcode <parthsit@gmail.com>
2015-08-13 15:43:40 +0530

    Create Legs.java

---
+++ Legs.java
@@ -0,0 +1,61 @@
+
+package rome2rio;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class Legs {
+
+    private String url;
+    private List<Hops> hops = new ArrayList<Hops>();
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * 
+     * @param url
+     *     The url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * 
+     * @return
+     *     The hops
+     */
+    public List<Hops> getHops() {
+        return hops;
+    }
+
+    /**
+     * 
+     * @param hops
+     *     The hops
+     */
+    public void setHops(List<Hops> hops) {
+        this.hops = hops;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
